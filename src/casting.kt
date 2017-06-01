3680812b1cfc815037a4cbd0c2b65d35e37edd17
Riashad <riashad.noor@gmail.com>
2017-06-02 03:12:29 +0600

    Casting Practice

---
+++ src/casting.kt
@@ -0,0 +1,25 @@
+/**
+ * Created by Riashad on 6/2/2017.
+ */
+
+fun main(args: Array<String>){
+    /**
+     * Casting Practice
+     * */
+
+    var a :Int = 5
+    var b :Double= 10.25
+    var c :Char= 'A'
+
+    //Integer to Double
+    println("Int to Double: "+ a.toDouble())
+
+    //Double to Integer
+    println("Double to Int: "+b.toInt())
+
+    //Integer to char
+    println("Int to Char: " +65.toChar())
+
+    //Char to Int
+    println("Char to Int: "+c.toInt())
+}
